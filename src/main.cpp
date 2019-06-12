9347fd2132af4079904f9e9cbb47402a96ef6a38
Mauricio Bustos <m@bustos.org>
2019-06-12 07:19:44 +0000

    expanding to full sculpture

--- src/main.cpp
+++ src/main.cpp
@@ -21,24 +21,37 @@
 #include <SPI.h>
 #include <TidesControllerClient.h>
 
-#define DEVICE_NUMBER (2) // 1-5
-#define MAC_LOCAL
-const byte MAC[2][6] = {
+#define DEVICE_TOTAL (7)
+#define DEVICE_NUMBER (2) // 1-7
+//#define MAC_LOCAL
+const byte MAC[DEVICE_TOTAL][DEVICE_TOTAL] = {
+  {0x04, 0xE9, 0xE5, 0x07, 0xDD, 0x65},
+  {0x04, 0xE9, 0xE5, 0x07, 0xDD, 0x68},
   {0x04, 0xE9, 0xE5, 0x07, 0xDD, 0x7D},
-  {0x04, 0xE9, 0xE5, 0x07, 0xDD, 0x65}
+  {0x04, 0xE9, 0xE5, 0x07, 0xDA, 0x00},
+  {0x04, 0xE9, 0xE5, 0x07, 0xDA, 0x00},
+  {0x04, 0xE9, 0xE5, 0x07, 0xDA, 0x00},
+  {0x04, 0xE9, 0xE5, 0x07, 0xDA, 0x00}
 };
 #ifdef MAC_LOCAL
 const IPAddress IP[] = {
-  IPAddress(169, 254, 18, 81), 
-  IPAddress(169, 254, 18, 82)
+  IPAddress(10, 0, 1, 31), 
+  IPAddress(10, 0, 1, 32), 
+  IPAddress(192, 168, 7, 95), 
+  IPAddress(10, 0, 1, 34) 
 };
-const IPAddress SERVER(169, 254, 18, 80);
+const IPAddress SERVER(192, 168, 7, 91);
 #else
-const IPAddress IP[] = {
-  IPAddress(192, 168, 7, 111),
-  IPAddress(192, 168, 7, 112)
+const IPAddress IP[DEVICE_TOTAL] = {
+  IPAddress(192, 168, 0, 10),
+  IPAddress(192, 168, 0, 11),
+  IPAddress(192, 168, 0, 12),
+  IPAddress(192, 168, 0, 13),
+  IPAddress(192, 168, 0, 14),
+  IPAddress(192, 168, 0, 15),
+  IPAddress(192, 168, 0, 16)
 };
-const IPAddress SERVER(192, 168, 7, 101);
+const IPAddress SERVER(192, 168, 0, 1);
 #endif
 const int PORT = 1999;
 
@@ -49,7 +62,7 @@ TidesControllerClient client;
 
 #define PIR_PIN A1
 #define PIR_LED_PIN 2
-#define PIR_THRESHOLD 128
+#define PIR_THRESHOLD 512
 
 bool pirState = false;
 int PIRValue;
@@ -58,6 +71,7 @@ int IRSensorValue;
 void setup() {
   pinMode(PIR_PIN, INPUT);
   pinMode(PIR_LED_PIN, OUTPUT);
+  //digitalWrite(PIR_LED_PIN, HIGH);
 
   client = TidesControllerClient(IP[DEVICE_NUMBER - 1], SERVER, PORT, (byte*)MAC[DEVICE_NUMBER - 1], DEVICE_NUMBER);
 
